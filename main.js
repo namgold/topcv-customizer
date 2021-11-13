db9bb641a4287eac9b9bfd0a239d337ab14e0833
Nam Nguyen <hnamnguyencse@gmail.com>
2021-11-14 03:42:30 +0700

    new cv

--- main.js
+++ main.js
@@ -16,7 +16,10 @@ const topCVCustomizer = () => {
                 width: 73%
             }
             @media print {
-                #cvo-experience {page-break-before: always;}
+                #experience-table .row:nth-child(2) {
+                    page-break-before: always;
+                    margin-top: 20px;
+                }
             }
 
             span.code {
@@ -35,33 +38,39 @@ const topCVCustomizer = () => {
 
     $('#cvo-body').css('padding-bottom', 0)
 
-    $('.contact-item-label')[0].innerText = 'YEAR OF BIRTH'
-    $('.contact-item-label')[1].innerText = 'SEX'
-    $('.contact-item-label')[2].innerText = 'PHONE NUMBER'
-    $('.contact-item-label')[3].innerText = 'EMAIL'
-    $('.contact-item-label')[4].innerText = 'ADDRESS'
-    $('.contact-item-label')[5].innerText = 'WEBSITE'
-    const websiteLink = $('#cvo-profile-website')[0].innerText;
-    const formatedWebsiteLink = websiteLink.startsWith('http') ? websiteLink : 'http://' + websiteLink;
-    $('#cvo-profile-website')[0].innerHTML = '<a href = ' + formatedWebsiteLink + '>' + websiteLink + '</a>'
+    const translateInfoTitle = () => {
+        $('.contact-item-label')[0].innerText = 'YEAR OF BIRTH'
+        $('.contact-item-label')[1].innerText = 'SEX'
+        $('.contact-item-label')[2].innerText = 'PHONE NUMBER'
+        $('.contact-item-label')[3].innerText = 'EMAIL'
+        $('.contact-item-label')[4].innerText = 'ADDRESS'
+        $('.contact-item-label')[5].innerText = 'WEBSITE'
+    }
+    translateInfoTitle();
 
+    const formatWebsiteLink = () => {
+        const websiteLink = $('#cvo-profile-website')[0].innerText;
+        const formatedWebsiteLink = websiteLink.startsWith('http') ? websiteLink : 'http://' + websiteLink;
+        $('#cvo-profile-website')[0].innerHTML = '<a href = ' + formatedWebsiteLink + '>' + websiteLink + '</a>'
+    }
+    formatWebsiteLink();
 
-    let skills = $('#cvo-additional-information-details').contents().filter(function() {return this.nodeType == Node.TEXT_NODE;});
-    let skillsHTML = '';
-    $('#cvo-additional-information-details').contents().each((index, i) => {
-        if (i.nodeType == Node.TEXT_NODE) {
-            skillsHTML += i.textContent.replace(/&nbsp;/g, '').split(',').map(i => '<span class="code">' + i.trim() + '</span>').join('');
-        } else if (i.nodeName == 'B') {
-            skillsHTML += '</div><div>' + i.outerHTML.replace(/&nbsp;/g, '') + ':' ;
-        } else if (i.nodeName == 'BR') {
-        } else {
-            // skillsHTML += i.outerHTML.replace(/&nbsp;/g, '') + ':';
-        }
-    })
-
-    document.getElementById('cvo-additional-information-details').innerHTML = '<div>' + skillsHTML + '</div>';
+    const skillHighlight = () => {
+        let skills = $('#cvo-additional-information-details').contents().filter(function() {return this.nodeType == Node.TEXT_NODE;});
+        let skillsHTML = '';
+        $('#cvo-additional-information-details').contents().each((index, i) => {
+            if (i.nodeType == Node.TEXT_NODE) {
+                skillsHTML += i.textContent.replace(/&nbsp;/g, '').split(',').map(i => '<span class="code">' + i.trim() + '</span>').join('');
+            } else if (i.nodeName == 'B') {
+                skillsHTML += '</div><div>' + i.outerHTML.replace(/&nbsp;/g, '') + ':' ;
+            } else if (i.nodeName == 'BR') {
+            } else {
+                // skillsHTML += i.outerHTML.replace(/&nbsp;/g, '') + ':';
+            }
+        })
+        document.getElementById('cvo-additional-information-details').innerHTML = '<div>' + skillsHTML + '</div>';
+    }
+    // skillHighlight();
 }
-
-topCVCustomizer
 
 topCVCustomizer();
